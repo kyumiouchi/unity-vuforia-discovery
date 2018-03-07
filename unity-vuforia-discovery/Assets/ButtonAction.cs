aacac9c76f1464869b4324dc282936d5f16743e8
yumiouchi <yumiouchidesu@gmail.com>
2018-03-06 23:59:35 -0400

    Add UI elements at Scene "clickChangeActionRobot" and button action at 3dObject

---
+++ unity-vuforia-discovery/Assets/ButtonAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAction : MonoBehaviour {
+
+
+    public Button attackButton;
+    public GameObject hose;
+	// Use this for initialization
+	void Start () {
+        attackButton.onClick.AddListener(attackButtonDown);
+	}
+
+    private void attackButtonDown()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        hose.transform.Find("WaterShower").gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+
+        hose.transform.Find("WaterShower").gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
